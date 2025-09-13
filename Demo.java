fba88167272fb4f2d9283fc8b9291bb7d5f625bb
AlinaKazmi <9alinakazmi@gmail.com>
2025-09-13 11:56:29 +0530

    Demo.java

--- Demo.java
+++ Demo.java
@@ -8,8 +8,14 @@ class ThisDemo
         b=y;
 
     }
-}
-    class Demo{
+    public void put();
+    {
+        System.out.println("a=" +a);
+        System.out.println("b=" +b);
+    }
+
+    class Demo
+    {
         public static void main(String[] args)
         {
             ThisDemo object=new ThisDemo();
