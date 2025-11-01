01a6c2e561ce441501eda2598f26331241876235
AlinaKazmi <9alinakazmi@gmail.com>
2025-11-01 15:08:44 +0530

    Create StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class length
+{
+    public static void main(String [] args)
+    {
+        String s = "Programming";
+System.out.println(s.length());// Hello World
+    }
+}
