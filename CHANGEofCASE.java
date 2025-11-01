9eef3dee645fadce43b4cad423eae2bdc2ba4ff2
AlinaKazmi <9alinakazmi@gmail.com>
2025-11-01 15:17:31 +0530

    Create CHANGEofCASE.java

---
+++ CHANGEofCASE.java
@@ -0,0 +1,11 @@
+import java .util.*;
+public class ChangeOfCase
+{
+    public static void main(String []args)
+    {
+     String s = "Hello";
+System.out.println(s.toUpperCase()); 
+System.out.println(s.toLowerCase());
+    }
+}
+
