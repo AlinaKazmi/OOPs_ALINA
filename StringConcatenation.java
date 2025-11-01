65d88583d12bfd0ce325901c31bb88a18fa39dbf
AlinaKazmi <9alinakazmi@gmail.com>
2025-11-01 15:06:04 +0530

    Create StringConcatenation.java

---
+++ StringConcatenation.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class concatenation
+{
+    public static void main(String [] args)
+    {
+        String s1 = "Hello";
+String s2 = "World";
+String s3 = s1 + " " + s2; // Using + operator
+String s4 = s1.concat(" " + s2); // Using concat() method
+System.out.println(s3); // Hello World
+    }
+}
