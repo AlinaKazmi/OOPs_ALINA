c34cfdc5787bf1b2526c5b5e0f2685210b64a146
AlinaKazmi <9alinakazmi@gmail.com>
2025-11-01 15:26:58 +0530

    Create StringSearching.java

---
+++ StringSearching.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class StringSearching 
+{
+    public static void main(String[] args) 
+    {
+        String s = "Programming";
+System.out.println(s.indexOf('g')); 
+System.out.println(s.lastIndexOf('g')); 
+System.out.println(s.contains("gram"));
+}
+        
+}
