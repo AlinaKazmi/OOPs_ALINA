f01453e16ed8c6e646ee6cfcaa3d455ef5b85fb4
AlinaKazmi <9alinakazmi@gmail.com>
2025-11-01 15:15:16 +0530

    Create Substring.java

---
+++ Substring.java
@@ -0,0 +1,9 @@
+public class substring
+{
+    public static void main(String []args)
+    {
+       String s = "Computer";
+System.out.println(s.substring(3)); 
+System.out.println(s.substring(0, 4));
+    }
+}
